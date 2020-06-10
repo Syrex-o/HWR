f7f12e833b8b503330df76c2dac839e938d5b66b
Syrex-o <alberthenkel.ah@gmail.com>
2020-06-10 07:27:43 +0200

    Exercise 3

--- mapreduce/mapper.py
+++ mapreduce/mapper.py
@@ -35,7 +35,10 @@ for line in sys.stdin:
     data = line.strip().split("\t")
 
     # store the 6 elements of the tuple in seperate variables
-    date, time, item, category, sales, payment = data
+    if len(data)==6:# store the 6 elements of the tuple in seperate variables
+    	date, time, item, category, sales, payment = data
+    else:
+	raise Exception('Not 6 elements')
 
     # Write the key-value combination to standard output (stdout)
     # Key is the payment, value is the sales     
